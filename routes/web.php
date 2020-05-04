4656c854cbb885c77b6caa8c356ad62f22569045
Ashish Shakya <ashishakya@outlook.com>
2020-05-04 21:52:41 +0545

    Add UserSeeder and add auth middleware

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Route::get('dashboard', [DashboardController::class, '__invoke']);
 
 Auth::routes(['register'=>false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware'=>'auth'], function (){
+    Route::get('dashboard', [DashboardController::class, '__invoke']);
+});
