7eca97dc3961c2b2e153a9b180ec368d52a8e8bf
Ashish Shakya <ashishakya@outlook.com>
2020-05-05 09:42:19 +0545

    Add navigation component

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>
-        <p>This is Inertia js dashboard</p>
-        <hr>
-        <p>Welcome {{ fname }} {{ lname }}</p>
-    </div>
+    <layout>
+<!--        these are replaced inside slot tag in Layout.vue file-->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>This is dashboard</p>
+                    Hello {{ fname }} {{ lname }}
+                </div>
+            </div>
+        </div>
+    </layout>
 </template>
 
 <script>
+    import Layout from "../../Shared/Layout";
+
     export default {
-        props:['fname', 'lname']
+        props:['fname', 'lname'],
+        components:{
+            Layout
+        }
     }
 </script>
